be173b9de194bd84fb870a702b14fe4ae115f848
GermanSincovic <german.sincovic.92@gmail.com>
2021-05-06 22:49:36 +0300

    JsonUtils

--- src/main/java/com/allure/utils/JsonUtils.java
+++ src/main/java/com/allure/utils/JsonUtils.java
@@ -2,7 +2,6 @@ package com.allure.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 
 import java.io.IOException;
 
